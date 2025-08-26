802ba20be75401ff60909ff66aa8021105471c38
𐔌՞꜆.  ̫.꜀՞𐦯 <pv.sofiadulce@gmail.com>
2025-08-26 12:11:46 -0300

    Update ProgramaVectores.pas
    dfsfsdf

--- ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 0 /ProgramaVectores.pas
+++ ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 0 /ProgramaVectores.pas
@@ -6,7 +6,7 @@ aleatoriamente (entre un mínimo y máximo recibidos por parámetro),
 deben ser almacenados en el vector en el mismo orden que se
 generaron, hasta que se genere el cero.
 
-b) Implemente un módulo ImprimirVector que reciba el vector generago
+b) Implemente un módulo ImprimirVector que reciba el vector generado
 en a) e imprima todos los valores del vector en el mismo orden que
 están almacenados. Qué cambiaría para imprimir en orden inverso?
 
