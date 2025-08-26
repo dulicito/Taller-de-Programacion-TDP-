d40425f0f2f520f2b6ac9a404bc7ce5d84bac1a7
𐔌՞꜆.  ̫.꜀՞𐦯 <pv.sofiadulce@gmail.com>
2025-08-26 12:22:21 -0300

    Create Gimnasio.pas
    gym

---
+++ ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 0 /Gimnasio.pas
@@ -0,0 +1,21 @@
+{ un gimnasio necesita procesar las asistencias de sus clientes. cada
+  asistencia tiene dia, mes, año, numero de cliente(entre 1 y 500) y la 
+  actividad realizada(valor entre 1 y 5).
+  a) implemente un modulo que retorne una lista de asistencia de clientes a
+  un gimnasio. las asistencias dentro de la lista deben quedar ordenadas de 
+  menor a mayor por numero de cliente. generar aleatoriamente los valores hasta
+  generar un valor cero para el numero de cliente.
+  b) implemente un modulo que reciba la lista generada en a) e imprima todos
+  los valores de la lista en el mismo orden que estan almacenados.
+  c) implemente un modulo que reciba la lista generada en a) y un numero de 
+  cliente y retorne la cantidad de asistencias del cliente recibido. Mostrar
+  el resultado desde el programa principal.
+  d) implemente un modulo que reciba la lista generada en a) y retorne la 
+  actividad con mayor cantidad de asistencias. Mostrar el resultado desde
+  el programa principal. }
+
+
+program gimnasio;
+begin
+  
+end.
