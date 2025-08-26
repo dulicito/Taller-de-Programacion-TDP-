41133ca2e40401ca9a735c6838713d88a9648b32
𐔌՞꜆.  ̫.꜀՞𐦯 <pv.sofiadulce@gmail.com>
2025-08-26 19:20:28 +0000

    ej2

---
+++ ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 1/ej2p1.pas
@@ -0,0 +1,88 @@
+{   2.- El administrador de un edificio de oficinas cuenta, en papel, con la información del pago de
+    las expensas de dichas oficinas.
+    Implementar un programa que invoque a módulos para cada uno de los siguientes puntos:
+    
+    a. Genere un vector, sin orden, con a lo sumo las 300 oficinas que administra. De cada oficina
+    se ingresa el código de identificación, DNI del propietario y valor de la expensa. La lectura
+    finaliza cuando se ingresa el código de identificación -1, el cual no se procesa.
+    
+    b. Ordene el vector, aplicando el método de inserción, por código de identificación de la
+    oficina.
+    
+    c. Ordene el vector aplicando el método de selección, por código de identificación de la oficina}
+    
+    program ej2p1;
+    const
+        maxofi: 300;
+        corte: -1;
+    type
+        oficina = record
+            codigo:integer;
+            dni:integer;
+            valorexpensa:real;
+        end;
+        vector= array[1..maxofi] of oficina;
+        
+
+    procedure leeroficina(var r: oficina)
+    begin
+        readln(r. codigo);
+        if (r.codigo <> -1)then 
+            readln(r.dni);
+            readln(r.valorexpensa);
+        end; 
+    end;
+    procedure generarVector (var v:vector; var dl:integer); //INCISO A 
+    var 
+        r:oficina; 
+    begin
+        dl:= 0; 
+        leeroficina(r);
+        while (r.codigo <> -1) or (dl < maxofi)do begin
+            dl:= dl + 1;
+            v[dl]:= r;
+            leeroficina(r);
+        end; 
+    end;
+        
+    procedure inserccion (var v:vector; dl:integer); //MODULO B 
+    var
+        i,j:integer;
+        actual:oficina; //actual es del tipo de datos que guarda el vector
+     begin
+        for (i:= 2 to dl) do begin
+            actual:=v[i];
+            j:=i-1;
+            while (j>0) and (v[j].codigo > actual.codigo)do begin
+                v[j+1]:=v[j];
+                j:=j-1;
+             end;
+             v[j+1]:=actual;
+        end;
+     end;
+
+    procedure seleccion (var v:vector; dl:integer;) //MODULO C 
+    Var
+        i,j,pos:integer;
+        item:oficina; //item es del tipo de datos que guarda el vector
+    begin
+        for i:=1 to dl -1 do begin
+            pos:=i;
+            for j:= i+1 to dl do begin
+                if (v[j].codigo < v[pos].codigo) then
+                    pos:=j;
+                item:=v[pos];
+                v[pos]:=v[i];
+                v[i]:=item;
+             end;
+         end;
+    end;
+    
+    
+    var
+        v: vector; dl:integer;
+    begin 
+        generarVector(v,dl);//modulo a 
+        inserccion(v,dl);//modulo b 
+        seleccion(v,dl);//modulo c 
+    end;
