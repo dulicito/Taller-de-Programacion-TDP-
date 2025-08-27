3d025cf42f82907f4e55882e088758bae5f28629
𐔌՞꜆.  ̫.꜀՞𐦯 <pv.sofiadulce@gmail.com>
2025-08-27 16:04:08 -0300

    Update Gimnasio.pas
    asdasdsd

--- ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 0 /Gimnasio.pas
+++ ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 0 /Gimnasio.pas
@@ -14,7 +14,6 @@
   actividad con mayor cantidad de asistencias. Mostrar el resultado desde
   el programa principal.}
 
-nota: esta mal ksjdjsdk, despues lo corrijo
 
 program gimnasio;
 const
@@ -25,7 +24,7 @@ const
 type
     dias: 1..maxdia;
     meses: 1..maxmes;
-    clientes: 1..maxcli;
+    clientes: 0..maxcli; //nota: incluye desde el 0 por el corte de control del inciso a
     acts: 1..maxact;
     Asistencia: record
         dia:dias;
@@ -39,6 +38,58 @@ type
             info:Asistencia;
             sig: listaclientes;
     vectoractividad: array[acts]of integer;
+    
+{
+retornar: lista de asistencias
+debe quedar ordenadas de menor a mayor por numero de cliente
+generar los valores aleatoriamente hasta que numero de cliente = 0
+*  en eso usar un while }
+
+procedure cargarlista (var l:lista; var v:vectoractividad) //INCISO A
+var
+	r:Asistencia;
+begin
+	inicializarvector(v);
+	leerregistro(r); 
+	while (r.numcli <> 0)do begin
+		v[r.act]:= v[r.act] + 1;
+	    insertarordenado(l,r);
+	    leerregistro(r);
+	end;
+	
+end;
+
+procedure imprimir (l:lista) //MODULO B 
+begin
+	while (l<> nil)do begin
+		imprimirregistro(l^.info); //le paso el registro como parametro 
+		l:= l^.sig;
+	end;
+
+function asistenciasdelcliente(l:lista; num:integer):integer; //MODULO C
+var
+	ok: boolean; cant:integer;
+begin
+	cant:= 0;
+	while(l<>nil) do begin
+		if (l^.info.numcli = num) then 
+			cant:= cant + 1;
+		l:= l^.sig;
+	end;
+	asistenciasdelcliente:= cant;
+end;
+
+function max(v:vector):integer; //MODULO D
+var
+	i,maxx:integer;
+begin
+	for i:= 1 to 5 to begin
+		if (v[i] > maxx)then
+			maxx:= v[i];
+	end;
+	max:= maxx;
+end;
+
 var
     l: listaclientes; v: vectoractividad; cant,num: integer; 
 begin
@@ -47,7 +98,6 @@ begin
   cargarlista(l,v)//modulo A , nota: inicializar vector en el modulo 
   imprimir(l)//modulo B 
   writeln('Ingrese un numero de cliente'); readln(num);
-  asistenciasdelcliente(l,num,cant);//modulo C 
-  writeln('la cantidad de asistencias del cliente:' ,num,', es',cant); //modulo C 
-  writeln('la actividad con mayor asistencias es: ',max(v));
+  writeln('la cantidad de asistencias del cliente:' ,num,', es',asistenciasdelcliente(l,num,cant); //modulo C 
+  writeln('la actividad con mayor asistencias es: ',max(v));//modulo D 
 end.
