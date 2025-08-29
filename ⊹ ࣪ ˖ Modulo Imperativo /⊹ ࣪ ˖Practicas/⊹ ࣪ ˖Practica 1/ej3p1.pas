54e7911d6b697af45c677c461134f6a0fcea7254
𐔌՞꜆.  ̫.꜀՞𐦯 <pv.sofiadulce@gmail.com>
2025-08-29 11:54:56 -0300

    Update ej3p1.pas

--- ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 1/ej3p1.pas
+++ ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 1/ej3p1.pas
@@ -1,5 +1,4 @@
-{  
-3.- Netflix ha publicado la lista de películas que estarán disponibles durante el mes de
+{3.- Netflix ha publicado la lista de películas que estarán disponibles durante el mes de
 septiembre de 2025. De cada película se conoce: código de película, código de género (1:
 acción, 2: aventura, 3: drama, 4: suspenso, 5: comedia, 6: bélico, 7: documental y 8: terror) y
 puntaje promedio otorgado por las críticas.
@@ -12,8 +11,7 @@ obtenido entre todas las críticas, a partir de la estructura generada en a)..
 c. Ordene los elementos del vector generado en b) por puntaje utilizando alguno de los dos
 métodos vistos en la teoría.
 d. Muestre el código de película con mayor puntaje y el código de película con menor puntaje,
-del vector obtenido en el punto c). 
-}
+del vector obtenido en el punto c). }
 
 //hacer luego :p
 program ej3p1;
