e03f6a87c2066bbc65f42ee4b62604e964e43c35
𐔌՞꜆.  ̫.꜀՞𐦯 <pv.sofiadulce@gmail.com>
2025-08-29 11:54:20 -0300

    Update Gimnasio.pas
    dasdas

--- ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 0 /Gimnasio.pas
+++ ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 0 /Gimnasio.pas
@@ -17,87 +17,152 @@
 
 program gimnasio;
 const
-    maxcli: 500;
-    maxact: 5;
-    maxdia: 31;
-    maxmes: 12;
+    maxcli = 500;
+    maxact = 5;
+    maxdia = 31;
+    maxmes = 12;
 type
-    dias: 1..maxdia;
-    meses: 1..maxmes;
-    clientes: 0..maxcli; //nota: incluye desde el 0 por el corte de control del inciso a
-    acts: 1..maxact;
-    Asistencia: record
-        dia:dias;
-        mes:meses;
-        anio:integer;
-        numcli:clientes;
+    dias = 1..maxdia;
+    meses = 1..maxmes;
+    clientes = 0..maxcli; // incluye el 0 para el corte
+    acts = 1..maxact;
+
+    Asistencia = record
+        dia: dias;
+        mes: meses;
+        anio: integer;
+        numcli: clientes;
         act: acts;
     end;
-    listaclientes=  ^nodo
-        nodo= record
-            info:Asistencia;
-            sig: listaclientes;
-    vectoractividad: array[acts]of integer;
-    
-{
-retornar: lista de asistencias
-debe quedar ordenadas de menor a mayor por numero de cliente
-generar los valores aleatoriamente hasta que numero de cliente = 0
-*  en eso usar un while }
 
-procedure cargarlista (var l:lista; var v:vectoractividad) //INCISO A
+    lista = ^nodo;
+    nodo = record
+        info: Asistencia;
+        sig: lista;
+    end;
+
+    vectoractividad = array[acts] of integer;
+
+{------------------- PROCEDIMIENTOS AUXILIARES -------------------}
+
+procedure inicializarvector(var v: vectoractividad);
 var
-	r:Asistencia;
+	i: integer;
 begin
-	inicializarvector(v);
-	leerregistro(r); 
-	while (r.numcli <> 0)do begin
-		v[r.act]:= v[r.act] + 1;
-	    insertarordenado(l,r);
-	    leerregistro(r);
-	end;
-	
+    for i := 1 to maxact do
+        v[i] := 0;
 end;
 
-procedure imprimir (l:lista) //MODULO B 
+procedure leerregistro(var r: Asistencia);
 begin
-	while (l<> nil)do begin
-		imprimirregistro(l^.info); //le paso el registro como parametro 
-		l:= l^.sig;
-	end;
-
-function asistenciasdelcliente(l:lista; num:integer):integer; //MODULO C
-var
-	ok: boolean; cant:integer;
-begin
-	cant:= 0;
-	while(l<>nil) do begin
-		if (l^.info.numcli = num) then 
-			cant:= cant + 1;
-		l:= l^.sig;
-	end;
-	asistenciasdelcliente:= cant;
+    r.numcli := random(maxcli+1); // genera entre 0 y 500
+    if r.numcli <> 0 then begin
+        r.dia := random(maxdia)+1;
+        r.mes := random(maxmes)+1;
+        r.anio := 2025;
+        r.act := random(maxact)+1;
+    end;
 end;
 
-function max(v:vector):integer; //MODULO D
+procedure insertarordenado(var l: lista; r: Asistencia);
 var
-	i,maxx:integer;
+    ant, act, nue: lista;
 begin
-	for i:= 1 to 5 to begin
-		if (v[i] > maxx)then
-			maxx:= v[i];
-	end;
-	max:= maxx;
+    new(nue);
+    nue^.info := r;
+    ant := nil;
+    act := l;
+    while (act<>nil) and (act^.info.numcli < r.numcli) do begin
+        ant := act;
+        act := act^.sig;
+    end;
+    if ant = nil then begin
+        nue^.sig := l;
+        l := nue;
+    end
+    else begin
+        ant^.sig := nue;
+        nue^.sig := act;
+    end;
 end;
 
-var
-    l: listaclientes; v: vectoractividad; cant,num: integer; 
+procedure imprimirregistro(r: Asistencia);
 begin
-  l:= nil; //inicializo la lista en nil 
-  cant:= 0
-  cargarlista(l,v)//modulo A , nota: inicializar vector en el modulo 
-  imprimir(l)//modulo B 
-  writeln('Ingrese un numero de cliente'); readln(num);
-  writeln('la cantidad de asistencias del cliente:' ,num,', es',asistenciasdelcliente(l,num,cant); //modulo C 
-  writeln('la actividad con mayor asistencias es: ',max(v));//modulo D 
+    writeln('Cliente: ', r.numcli,
+            ' | Fecha: ', r.dia,'/',r.mes,'/',r.anio,
+            ' | Actividad: ', r.act);
+end;
+
+{------------------- INCISO A -------------------}
+procedure cargarlista(var l: lista; var v: vectoractividad);
+var
+    r: Asistencia;
+begin
+    inicializarvector(v);
+    leerregistro(r);
+    while (r.numcli <> 0) do begin
+        v[r.act] := v[r.act] + 1;
+        insertarordenado(l, r);
+        leerregistro(r);
+    end;
+end;
+
+{------------------- INCISO B -------------------}
+procedure imprimir(l: lista);
+begin
+    while (l <> nil) do begin
+        imprimirregistro(l^.info);
+        l := l^.sig;
+    end;
+end;
+
+{------------------- INCISO C -------------------}
+function asistenciasdelcliente(l: lista; num: integer): integer;
+var
+    cant: integer;
+begin
+    cant := 0;
+    while (l <> nil) do begin
+        if (l^.info.numcli = num) then
+            cant := cant + 1;
+        l := l^.sig;
+    end;
+    asistenciasdelcliente := cant;
+end;
+
+{------------------- INCISO D -------------------}
+function maxactividad(v: vectoractividad): integer;
+var
+    i, maxval, pos: integer;
+begin
+    maxval := -1;
+    pos := -1;
+    for i := 1 to maxact do begin
+        if v[i] > maxval then begin
+            maxval := v[i];
+            pos := i;
+        end;
+    end;
+    maxactividad := pos;
+end;
+
+{------------------- PROGRAMA PRINCIPAL -------------------}
+var
+    l: lista;
+    v: vectoractividad;
+    num: integer;
+begin
+    randomize; //nunca olvidar de declarar este randomize ;-;
+    l := nil;
+    cargarlista(l, v); // inciso A
+    writeln('--- LISTA DE ASISTENCIAS ---');
+    imprimir(l); // inciso B
+
+    writeln; writeln('Ingrese un numero de cliente: '); readln(num);
+    writeln('El cliente ', num, ' tuvo ',
+            asistenciasdelcliente(l, num),
+            ' asistencias.'); // inciso C
+
+    writeln('La actividad con mayor cantidad de asistencias es: ',
+            maxactividad(v)); // inciso D
 end.
