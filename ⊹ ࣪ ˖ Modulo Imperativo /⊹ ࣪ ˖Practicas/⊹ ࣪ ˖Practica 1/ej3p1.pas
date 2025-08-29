cea79ab4e5ccebcd5cce26106ecd8f50bdeb6521
𐔌՞꜆.  ̫.꜀՞𐦯 <pv.sofiadulce@gmail.com>
2025-08-29 12:34:16 -0300

    Update ej3p1.pas

--- ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 1/ej3p1.pas
+++ ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 1/ej3p1.pas
@@ -13,7 +13,8 @@ métodos vistos en la teoría.
 d. Muestre el código de película con mayor puntaje y el código de película con menor puntaje,
 del vector obtenido en el punto c). }
 
-//nota: revisar, se usa un vector de listas
+//nota: revisar, se usa un vector de listas !!!!!
+//nota: cuando diga ordenado por x criterio y agrupado por x criterio siempre va a ser un vector de listas 
 program ej3p1;
 const
 	maxgen: 8;
@@ -26,7 +27,87 @@ type
 		puntaje_prom:real;
 	end;
 	
+	listapeliculas= ^nodo
+		nodo=record
+			info:pelicula;
+			sig:listapeliculas;
+		end;
+	vector= array[generos]of real;
+	
+{------------------MODULOS-----------------------------}
+
+{a. Lea los datos de películas, los almacene por orden de llegada y agrupados por código de
+género, y retorne en una estructura de datos adecuada. La lectura finaliza cuando se lee el
+código de la película -1.}
+
+procedure generarestructura (var l:listapeliculas)
+var
+	r:pelicula;
+begin
+	leerregistro(r);
+	while (r.codigo <> 0)do begin
+		insertarordenado(l,r);
+		leerregistro(r);
+	end;
+end;
+
+{b. Genere y retorne en un vector, para cada género, el código de película con mayor puntaje
+obtenido entre todas las críticas, a partir de la estructura generada en a)..
+* 
+* pseudocodigo:
+* objetivo: 
+* 	guardar en cada pos del vector el codigo de pelicula con mayor puntaje (>puntaje_prom)
+* 
+* aviso: debe haber un corte de control ya que la lista esta ordenada por GENERO!!!!
+* 
+* algoritmo:
+* mientras la lista NO este vacia 
+* 	guardo el cod de generoactual 
+* 	inicializo max para usarlo solo por genero 
+* 	mientras sigo recorriendo la lista con el mismo cod de genero
+* 		si puntaje prom mayor que max
+* 			actualizo max 
+* 		paso al siguiente nodo 
+* 	guardo max en la pos del genero en el vector
+* paso al siguiente nodo 
+* }
+
+procedure generarvector(var l: listapeliculas; var v:vector);
 var
 begin
+	while (l <> nil)do begin
+		cod_act:= l^.info.codigo;
+		max:= -9999999;
+		while (l^.info.codigo = cod_act)do begin 
+			if (l^.info.puntaje_prom > max)then begin
+				max:= l^.info.puntaje_prom;
+				genero_Actual:= l^.info.cod_gen;
+			end;
+			l:= l^.sig;
+		end;
+		v[genero_Actual]:= max;
+		l:= l^.sig;
+	end;
+end;
+
+
+{c. Ordene los elementos del vector generado en b) por puntaje utilizando alguno de los dos
+métodos vistos en la teoría.}
+
+
+procedure ordenarvector(var v:vector);
+var
+begin
+end;
+
+{------------------PROGRAMA PRINCIPAL------------------}
+var
+	l:listapeliculas; v:vector;
+begin
+	l:= nil;
+	generarestructura(l); //inciso A 
+	generarvector(l,v);//Inciso B
+	ordenarvector(v);//Inciso C
+	imprimir(v);//Inciso D 
 end;
 
