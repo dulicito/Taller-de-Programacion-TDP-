f17a6db2fc68c8579d654bc2bd588a74c9e5f775
𐔌՞꜆.  ̫.꜀՞𐦯 <pv.sofiadulce@gmail.com>
2025-09-04 15:00:56 -0300

    Update and rename imprimirVector.pas to imprimirVectorRecursivo.pas

--- ⊹ ࣪ ˖ Algoritmos/⊹ ࣪ ˖ Modulo Imperativo/imprimirVectorRecursivo.pas
+++ ⊹ ࣪ ˖ Algoritmos/⊹ ࣪ ˖ Modulo Imperativo/imprimirVectorRecursivo.pas
@@ -1,6 +1,6 @@
 {imprime de adelante hacia atras}
 
-procedure imprimirVector(v:Vector; diml:integer);
+procedure imprimirVectorRecursivo(v:Vector; diml:integer);
 begin
   if (diml > 0)then begin
     imprimirVector(v, diml -1);
@@ -9,7 +9,7 @@ begin
 end;
 
 {imprime de atras hacia adelante}
-procedure imprimirVector(v:Vector; diml:integer);
+procedure imprimirVectorRecursivo(v:Vector; diml:integer);
 begin
   if (diml > 0)then begin
     writeln(v[diml]);
