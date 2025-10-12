40585c94e29e6b58ce4dcea6ace86a3122cc374b
𐔌՞꜆.  ̫.꜀՞𐦯 <pv.sofiadulce@gmail.com>
2025-10-12 22:33:07 +0000

    <

--- ⊹ ࣪ ˖ Modulo Objetos/⊹ ࣪ ˖ Practicas/TP_ProyectoAlumnos/tema3/ejercicio2/Demo01ConstructoresLibro.java
+++ ⊹ ࣪ ˖ Modulo Objetos/⊹ ࣪ ˖ Practicas/TP_ProyectoAlumnos/tema3/ejercicio2/Demo01ConstructoresLibro.java
@@ -1,24 +1,27 @@
 /*
 Demo que crea objetos Libro invocando a los constructores definidos. 
  */
-package tema3;
+package tema3.ejercicio2;
 
-/**
- *
- * @author vsanz
- */
+import tema3.ejercicio2.Libro;
+import tema3.ejercicio2.Autor;
+
+
 public class Demo01ConstructoresLibro {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Autor autor1 = new Autor();
+        Autor autor2 = new Autor();
+        
         Libro libro1= new  Libro( "Java: A Beginner's Guide",   
                                  "Mcgraw-Hill", 2014,   
-                                 "Herbert Schildt", "978-0071809252", 21.72);
+                                 autor1, "978-0071809252", 21.72);
         Libro libro2= new Libro("Learning Java by Building Android Games",  
                               "CreateSpace Independent Publishing", 
-                               "John Horton", "978-1512108347");
+                               autor2, "978-1512108347");
         System.out.println(libro1.toString());
         System.out.println(libro2.toString());
         System.out.println("Precio del libro2: " +libro2.getPrecio());
