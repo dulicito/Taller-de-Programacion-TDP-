2672fed7602506359a3c317b21e5075b67f3fa9e
🏻‎🏼‎🏽‎🏾🏿 .✦ ہ˖ <pv.sofiadulce@gmail.com>
2026-08-24 19:39:42 -0300

    ˚˖𓍢ִ໋🦢˚
    Updated comments for clarity and formatting in the Pascal program.

--- ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 2/ej5p2.pas
+++ ⊹ ࣪ ˖ Modulo Imperativo /⊹ ࣪ ˖Practicas/⊹ ࣪ ˖Practica 2/ej5p2.pas
@@ -1,14 +1,22 @@
-{5.- Realizar un programa que lea números y que utilice un módulo recursivo que escriba el
-equivalente en binario de un número decimal. El programa termina cuando el usuario ingresa
-el número 0 (cero).
+{5.- Realizar un programa que lea números y que utilice un módulo 
+recursivo que escriba el equivalente en binario de un número decimal. 
+El programa termina cuando el usuario ingresa el número 0 (cero).
 
-Ayuda: Analizando las posibilidades encontramos que: Binario (N) es N si el valor es menor a 2.
-¿Cómo obtenemos los dígitos que componen al número? ¿Cómo achicamos el número para la
-próxima llamada recursiva? Ejemplo: si se ingresa 23, el programa debe mostrar: 10111.}
-
+Ayuda: Analizando las posibilidades encontramos que: Binario (N) es N 
+si el valor es menor a 2.
+¿Cómo obtenemos los dígitos que componen al número? ¿Cómo achicamos el 
+número para la próxima llamada recursiva? Ejemplo: si se ingresa 23, el 
+programa debe mostrar: 10111.}
 
 program ej4p2;
 
+{ OBJETIVO:
+  Mostrar recursivamente el equivalente en binario
+  de un número decimal.
+
+  PARAMETRO:
+  n: número decimal que se desea convertir.
+* }
 procedure escribirBinario(n: integer);
 begin
   if (n < 2) then
